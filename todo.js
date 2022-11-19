f2c24f94458e7153468d0c924700db385d961681
shivasai2002 <76087708+shivasai2002@users.noreply.github.com>
2022-11-19 15:28:26 +0530

    Update todo.js

--- todo.js
+++ todo.js
@@ -26,5 +26,37 @@ const todoList = () => {
   };
   return { all, add, markAsComplete, overdue, dueToday, dueLater };
 };
+const toDisplayableList = (list) => {
+    // Format the To-Do list here, and return the output string as per the format given above.
+    // FILL YOUR CODE HERE
+    // ..
+    // ..
+    // ..
+    // return OUTPUT_STRING
+    var i;
+    var displayList = []
+    for (i=0;i<list.length;i++) {
+      if (list[i].completed === false) {
+        //`[ ] ${i.title} ${i.dueDate}`
+        if (list[i].dueDate===today) {
+          displayList.push(`[ ] ${list[i].title}`)
+        }
+        else {
+          displayList.push(`[ ] ${list[i].title} ${list[i].dueDate}`)
+        }
+      }
+      else {
+        // `[x] ${i.title} ${i.dueDate}`
+        if (list[i].dueDate===today) {
+          displayList.push(`[x] ${list[i].title}`)
+        }
+        else {
+          displayList.push(`[x] ${list[i].title} ${list[i].dueDate}`)
+        }          
+      }
+    }
+    
+    return displayList.join("\n");
+  }
 
 module.exports = todoList;
